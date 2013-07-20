8cd5a3028ba331f1c38306cc8c9acecc82be988b
David Johnson <david.johnson@imperial.ac.uk>
2013-07-20 16:17:16 +0100

    initial dump from archive

---
+++ src/uk/ac/rdg/acet/mico/content/ContentEventListener.java
@@ -0,0 +1,10 @@
+package uk.ac.rdg.acet.mico.content;
+
+/**
+ *
+ * @author David
+ */
+public interface ContentEventListener {
+//    public void transferEvent(ContentTransferEvent event);
+//    public void queryResultReceived(QueryResult queryResult);
+}
---
+++ src/uk/ac/rdg/acet/mico/messages/IMultipartMessageProcessor.java
@@ -0,0 +1,9 @@
+package uk.ac.rdg.acet.mico.messages;
+
+/**
+ *
+ * @author David
+ */
+public interface IMultipartMessageProcessor {
+    public MultipartMessage popMultipartMessage();
+}
